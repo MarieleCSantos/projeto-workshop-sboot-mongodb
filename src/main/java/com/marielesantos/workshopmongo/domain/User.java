34871aad98ff879283e7ab2fbeeb9eadec107bfb
Mariele Santos <marielecsantos@outlook.com>
2023-06-21 11:32:16 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/marielesantos/workshopmongo/domain/User.java
+++ src/main/java/com/marielesantos/workshopmongo/domain/User.java
@@ -4,6 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
@@ -11,8 +13,10 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
+@Document
 public class User implements Serializable {
 
+    @Id
     private String id;
     private String name;
     private String email;

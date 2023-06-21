ca84e70472bb6e55e75a6f0e091c2dffba0de260
Mariele Santos <marielecsantos@outlook.com>
2023-06-21 11:47:04 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/marielesantos/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.marielesantos.workshopmongo.config;
+
+import com.marielesantos.workshopmongo.domain.User;
+import com.marielesantos.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        List<User> list = Arrays.asList(maria, alex, bob);
+
+        userRepository.saveAll(list);
+    }
+}

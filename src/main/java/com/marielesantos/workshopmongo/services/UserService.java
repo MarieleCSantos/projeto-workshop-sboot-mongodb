9d24097f96214c3d5500739e9c428c2569c37625
Mariele Santos <marielecsantos@outlook.com>
2023-06-22 09:51:14 -0300

    Obtendo um usuário por id

--- src/main/java/com/marielesantos/workshopmongo/services/UserService.java
+++ src/main/java/com/marielesantos/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.marielesantos.workshopmongo.services;
 
 import com.marielesantos.workshopmongo.domain.User;
 import com.marielesantos.workshopmongo.repository.UserRepository;
+import com.marielesantos.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,10 @@ public class UserService {
     private UserRepository repo;
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
     }
 }

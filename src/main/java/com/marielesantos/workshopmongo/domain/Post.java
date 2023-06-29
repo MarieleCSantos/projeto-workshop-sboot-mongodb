c8eaba196852ba87656c5fabc2d99aadb603ffef
Mariele Santos <marielecsantos@outlook.com>
2023-06-28 22:22:34 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/marielesantos/workshopmongo/domain/Post.java
@@ -0,0 +1,29 @@
+package com.marielesantos.workshopmongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(of = "id")
+@Document
+public class Post implements Serializable {
+
+    @Id
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+
+}

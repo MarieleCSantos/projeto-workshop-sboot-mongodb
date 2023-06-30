906c1c7ca4c06ea7eca39d61054422fd93425e4a
Mariele Santos <marielecsantos@outlook.com>
2023-06-30 15:11:26 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/marielesantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marielesantos/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.marielesantos.workshopmongo.resources;
 
+import com.marielesantos.workshopmongo.domain.Post;
 import com.marielesantos.workshopmongo.domain.User;
 import com.marielesantos.workshopmongo.dto.UserDTO;
 import com.marielesantos.workshopmongo.services.UserService;
@@ -62,5 +63,11 @@ public class UserResource {
         obj = service.update(obj);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }

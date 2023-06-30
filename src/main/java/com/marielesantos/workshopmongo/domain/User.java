0b3acb563738da24225746c099db88e9be727dc4
Mariele Santos <marielecsantos@outlook.com>
2023-06-30 15:06:42 -0300

    Referenciando os posts do usuário

--- src/main/java/com/marielesantos/workshopmongo/domain/User.java
+++ src/main/java/com/marielesantos/workshopmongo/domain/User.java
@@ -5,9 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -20,6 +23,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(String id, String name, String email) {
         this.id = id;

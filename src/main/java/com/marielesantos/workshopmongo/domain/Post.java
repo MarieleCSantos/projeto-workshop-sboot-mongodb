1d14a4e6eedeacf36186e9664c705a23eb7b63ad
Mariele Santos <marielecsantos@outlook.com>
2023-06-30 15:30:09 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/marielesantos/workshopmongo/domain/Post.java
+++ src/main/java/com/marielesantos/workshopmongo/domain/Post.java
@@ -1,6 +1,7 @@
 package com.marielesantos.workshopmongo.domain;
 
 import com.marielesantos.workshopmongo.dto.AuthorDTO;
+import com.marielesantos.workshopmongo.dto.CommentDTO;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -10,10 +11,11 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Setter
 @Getter
 @EqualsAndHashCode(of = "id")
@@ -26,5 +28,13 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 }
